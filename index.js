ed0116b416fa8f8dea813449e2ec14a7b761de15
besart <besartshyti@gmail.com>
2017-02-26 10:34:21 +0200

    inital commit'

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(a,b) {
+  if(a < 5) {
+    return a + b;
+  } else {
+    return a - b;
+  }
+};
---
+++ test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var test = require('tape');
+var add = require('./index.js');
+
+test('add: should add two numbers when a < 5', function(t) {
+  var res = add(1,2);
+  t.equal(res,3,'addition 1 + 2 -> 3');
+  t.end();
+});
+
+test('add: should subtract two numbers when a > 5', function(t) {
+  var res = add(6,2);
+  t.equal(res,4,'addition 6 - 2 -> 4');
+  t.end();
+});
